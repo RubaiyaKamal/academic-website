eddba6a6d8eea290afb326dfab22cbbd77649f01
RubaiyaKamal <kh0102267@gmail.com>
2024-10-31 09:42:50 +0500

    academic

--- app/components/Progessor.tsx
+++ app/components/Progessor.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function Home() {
 
 return (
-  <div> 
+  <> 
   <div className="container flex relative h-[94vh] w-screen mt-4">
  {/*School name Main Page */}
      <div className="main flex flex-col relative bg-cyan-500 h-[94vh] w-screen">
@@ -21,16 +21,15 @@ return (
         </div>
 
 
-  <div className="img-app flex flex-row justify-end items-end m-auto ml-56 pb-16 w-[150vh] h-[15vh]">
+  <div className="flex flex-row justify-end items-end m-auto ml-56 pb-16 -mt-8 w-[150vh] h-[15vh]">
     <Image className='flex object-cover rounded-full border-4 border-black' width={400} height={500} src='https://images.unsplash.com/photo-1577896851231-70ef18881754?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='image'/>
   </div>
 
    </div> 
-
-  
-   </div>
+   
    
   </div>
+  </>
 );
 };
 
